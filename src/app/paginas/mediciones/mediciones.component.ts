403dbd0b9858331006ea97bcfac8e50d1e4726d2
Caleb Ventura <caleb.ventura.v@uni.pe>
2018-10-29 00:51:16 -0500

    Implementación de una base de datos en Firebase

--- src/app/paginas/mediciones/mediciones.component.ts
+++ src/app/paginas/mediciones/mediciones.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from '../../services/info-pagina.service';
 
 @Component({
   selector: 'app-mediciones',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicionesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public infomedicion:InfoPaginaService) { }
 
   ngOnInit() {
   }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,22 +10,35 @@ export class InfoPaginaService {
   info: infoPagina ={};
   cargada = false;
 
+  medidas: any[]=[];
+
   constructor(private http: HttpClient) { 
     //console.log('Servicio de info pagina listo');
 
     //Crear un archivo que lea el json creado y tomar sus propiedades y usarlo
     //Para eso necesito un modulo que haga peticiones http
     // con HttpClient ya está creado el servicio
+    this.cargarInfo();
+    this.cargarMedidas();
 
+        //el subscribe recibe una respuesta
+  }
+  private cargarInfo(){
     //esta solo es la definicion de dónde está la página, pero no la ejectuta
     this.http.get('assets/data/data-pagina.json')
         .subscribe(
           (resp: infoPagina) => {
             this.cargada = true;
             this.info = resp;
-            console.log(resp);
+            //console.log(resp);
           }
         );
-        //el subscribe recibe una respuesta
+  }
+  private cargarMedidas(){
+    this.http.get('https://iot-angular.firebaseio.com/medicion.json')
+        .subscribe((resp: any[]) => {
+            this.medidas = resp;
+            console.log(resp);
+          });
   }
 }
